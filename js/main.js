a48933b6a74347acb0ba2ab41681747baf4e9801
ELMIR.WEB <elmir.web@gmail.com>
2022-05-31 11:19:14 +0500

    идет работа

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+const DELIVERY_TARIFF = 75; // Стоимость за километр.
+const MINIMUM_COST = 500; // Минимальная стоимость.
+
 (() => {})();
+
+ymaps.ready(() => {
+  let myMap = new ymaps.Map("map", {
+    center: [45.060491, 38.97304],
+    zoom: 9,
+    controls: [],
+  });
+
+  // myMap.behaviors.disable(["scrollZoom"]);
+
+  // let routePanelControl = new ymaps.control.RoutePanel({
+  //   // Создадим панель маршрутизации.
+  //   options: {
+  //     // Добавим заголовок панели.
+  //     showHeader: true,
+  //     autofocus: false,
+  //     title:
+  //       "Укажите адрес загрузки и выгрузки — стоимость рассчитается автоматически",
+  //   },
+  // });
+
+  // routePanelControl.routePanel.options.set({
+  //   // Пользователь сможет построить только автомобильный маршрут.
+  //   types: {
+  //     auto: true,
+  //   },
+  // });
+
+  // myMap.controls.add(routePanelControl);
+});
